5f614993d138fb3fa943aa57450608ffa45af533
mhmdmarshoud34 <mhmdmarshoud34@gmail.com>
2025-02-14 21:26:31 +0200

    initial backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log('server here');
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDb from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+connectDb();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
+
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT: ${port}`);
+});
